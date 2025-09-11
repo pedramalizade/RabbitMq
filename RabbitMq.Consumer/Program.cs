50b0fd999af2f19c9851182d9cd85a7eb22df725
pedram <pedramalizade03@gmail.com>
2025-09-11 21:31:18 +0330

    Add Project With RabbitMq

---
+++ RabbitMq.Consumer/Program.cs
@@ -0,0 +1,30 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+var factory = new ConnectionFactory { HostName = "localhost" };
+using var connection = await factory.CreateConnectionAsync();
+using var channel = await connection.CreateChannelAsync();
+
+await channel.QueueDeclareAsync(
+    queue: "message",
+    durable: true,
+    exclusive: false,
+    autoDelete: false,
+    arguments: null);
+
+Console.WriteLine("Waiting for messages...");
+
+var consumer = new AsyncEventingBasicConsumer(channel);
+consumer.ReceivedAsync += async (sender, eventArgs) =>
+{
+    byte[] body = eventArgs.Body.ToArray(); 
+    string message = Encoding.UTF8.GetString(body);
+
+    Console.WriteLine($"Recived: {message}");
+
+   await ((AsyncEventingBasicConsumer)sender).Channel.BasicAckAsync(eventArgs.DeliveryTag, multiple: false);
+};
+
+await channel.BasicConsumeAsync("message", autoAck: false, consumer);
+Console.ReadLine();
---
+++ RabbitMq.Producer/Program.cs
@@ -0,0 +1,30 @@
+﻿using RabbitMQ.Client;
+using System.Data;
+using System.Text;
+
+var factory = new ConnectionFactory { HostName = "localhost" };
+using var connection = await factory.CreateConnectionAsync();
+using var channel = await connection.CreateChannelAsync();
+
+await channel.QueueDeclareAsync(
+    queue: "message",
+    durable: true,
+    exclusive: false,
+    autoDelete: false,
+    arguments: null);
+
+for (int i = 0; i < 10; i++)
+{
+    var message = $"{DataSetDateTime.Utc} - {Guid.CreateVersion7()}";
+    var body = Encoding.UTF8.GetBytes(message);
+
+    await channel.BasicPublishAsync(
+        exchange: string.Empty,
+        routingKey: "message",
+        mandatory: true,
+        basicProperties: new BasicProperties { Persistent = true },
+        body: body);
+
+    Console.WriteLine($"Sent: {message}");
+    await Task.Delay(2000);
+}
